f01727bca8fbbf52bdf51a656f99a1bb3a8caa72
Maedda63 <anais.bord63@gmail.com>
2019-04-10 14:32:13 +0200

    toutes les routes sont faites

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CharacterComponent } from './character/character.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { StarshipComponent } from './starship/starship.component';
+import { SpecieComponent } from './specie/specie.component';
+import { PlanetComponent } from './planet/planet.component';
+import { FilmComponent } from './film/film.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'character', pathMatch: 'full'},
   { path: 'character', component: CharacterComponent },
-  
+  { path: 'vehicle', component: VehicleComponent},
+  { path: 'starship', component: StarshipComponent},
+  { path: 'species', component: SpecieComponent},
+  { path: 'planet', component: PlanetComponent},
+  { path: 'film', component: FilmComponent}
 ];
 
 @NgModule({
