91b8513fd993abf49b09f1e698bab5413b22f662
Maedda63 <anais.bord63@gmail.com>
2019-04-10 11:53:14 +0200

    Creation of interface film/character/planet

--- src/app/model/character.model.ts
+++ src/app/model/character.model.ts
@@ -1,3 +1,15 @@
-export interface Character{
+import { Film } from './film.model';
+import { Specie } from './specie.model';
+import { Starship } from './starship.model';
+import { Vehicle } from './vehicle.model';
 
+export interface Character {
+    name: string;
+    birth_year: string;
+    gender: string;
+    films: Film[];
+    species: Specie[];
+    starships: Starship[];
+    vehicles: Vehicle[];
+    url: string;
 }
