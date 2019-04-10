fb378e12d6d8c396b1508745c0707156ef947455
PaHugon <pahugon@humanbooster.com>
2019-04-10 11:53:27 +0200

    Les compos by PA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SpecieComponent } from './specie/specie.component';
+import { StarshipComponent } from './starship/starship.component';
+import { CharacterComponent } from './character/character.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { FilmComponent } from './film/film.component';
+import { PlanetComponent } from './planet/planet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SpecieComponent,
+    StarshipComponent,
+    CharacterComponent,
+    VehicleComponent,
+    FilmComponent,
+    PlanetComponent
   ],
   imports: [
     BrowserModule,
