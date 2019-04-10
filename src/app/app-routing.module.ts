06cc1fa6eb20cf604219bf64f9d3da1b73a86a50
Maedda63 <anais.bord63@gmail.com>
2019-04-10 14:28:49 +0200

    path character

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharacterComponent } from './character/character.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'character', pathMatch: 'full'},
+  { path: 'character', component: CharacterComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
